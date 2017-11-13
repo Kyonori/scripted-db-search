cc5a7542b2dcc37f7066260e500f3695aa9dbe5d
kyonori1 <reustache2019@bklawtech.com>
2017-11-13 22:10:31 +0000

    first commit

--- db-search-functions.js
+++ db-search-functions.js
@@ -10,29 +10,79 @@ function getVersion(handleVersion) {
     // the database snapshot and passes it as an argument to the 'handleVersion'
     // callback.
     
-    //var version = snapshot.val();
-    //handleVersion(version);
+    var version = snapshot.val();
+    handleVersion(version);
   });
 }
 
 function getHelloWorldValue(handleType) {
   // TODO: Your code goes here. Hint: use 'getVersion' as a template.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/hello-world');
+  databaseVersionReference.once('value').then(function(snapshot){
+    
+    var value = snapshot.val();
+    handleType(value);
+    });
 }
 
 function applyDiscount(handleNewPrice) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/discount');
+  databaseVersionReference.once('value').then(function(snapshot){
+    
+    var newPrice = snapshot.val();
+    handleNewPrice(newPrice * 100);
+    
+    
+    });
+    
+    
 }
 
 function getArrayLength(handleLength) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot){
+    
+    var array = snapshot.val();
+    handleLength(array.length);
+    
+    
+    });
 }
 
 function getLastArrayElement(handleElement) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot){
+    
+    var array = snapshot.val();
+    handleElement(array[array.length-1]);
+    
+    
+    });
 }
 
 function getArrayJoin(handleJoin) {
   // TODO: Your code goes here.
+  
+  var database = firebase.database();
+  var databaseVersionReference = database.ref('/db-search/array');
+  databaseVersionReference.once('value').then(function(snapshot){
+    
+    var array = snapshot.val();
+    handleJoin(array.join());
+    
+    
+    });
 }
 
 function getNumberOrStringType(handleType) {
